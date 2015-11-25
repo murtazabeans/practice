264d07fa71d73ce34506e9941ca0cd3d071601b8
Murtaza <murtaza@IB-SEZ-PC-204.(none)>
2015-11-25 20:16:27 +0530

    My First commit

---
+++ practice.rb
@@ -0,0 +1,47 @@
+class Practice
+	def string_reverse
+		puts "Enter a string"
+		string = gets.chomp
+		word = string.split("")
+		for i in 0..word.length
+			print word[word.length-i]
+		end
+	end
+
+	def pattern
+		puts ""
+		numbers = "a".."z"
+		numbers.each_with_index do |number, index|
+			if (index+1)%3==0
+				print "#{number} \n"
+			else
+				print number
+			end
+		end
+	end
+
+	def palindrome
+		puts ""
+		string = gets.chomp
+		string_arr = string.split("")
+		reverse_string = string.reverse
+		reverse_arr = reverse_string.split("")
+		palindrome = false
+
+ 		for i in 0..(string.length - 1)
+			if string_arr[i] == reverse_arr[i]
+				palindrome = true
+			else
+				break
+			end	
+ 		end
+
+		puts palindrome ? "Palindrome" : "No"
+		puts string == reverse_string ? "Palindrome" : "No"
+	end	
+end
+
+obj = Practice.new
+obj.string_reverse
+obj.pattern
+obj.palindrome
